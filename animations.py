118a8c79a565aa73b2153261c563c7371c0338b8
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-20 16:59:20 +0500

    Add physics to spaceship movement

--- animations.py
+++ animations.py
@@ -3,6 +3,7 @@ import curses
 from random import randint, choice
 from itertools import cycle
 from frame_tools import get_frame_size, draw_frame, load_frame
+from physics import update_speed
 import global_vars
 
 
@@ -38,12 +39,15 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
 
 
 async def animate_spaceship(canvas, start_row, start_column, frame1, frame2,
-                            speed=1):
+                            row_speed=0, column_speed=0):
     rows, columns = canvas.getmaxyx()
     height, width = get_frame_size(frame1)
     for frame in cycle([frame2, frame1]):
-        start_row += global_vars.rows_direction * speed
-        start_column += global_vars.columns_direction * speed
+        row_speed, column_speed = update_speed(
+            row_speed, column_speed, global_vars.rows_direction,
+            global_vars.columns_direction, 5, 10)
+        start_row += row_speed
+        start_column += column_speed
         start_row = max(1, start_row)
         start_row = min(rows - 1 - height, start_row)
         start_column = max(1, start_column)
